777965fa0d300a4348fdd20b305e69d69363aee0
Artur Shaik <artur@shaik.link>
2022-05-12 14:33:39 +0600

    automatic install of jdtls

--- lua/jc/server.lua
+++ lua/jc/server.lua
@@ -1,18 +1,49 @@
-local servers = require "nvim-lsp-installer.servers"
-
 local M = {}
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+
+local function download_jdtls()
+    local servers = require("nvim-lsp-installer.servers")
+    local installer = require("nvim-lsp-installer")
+
+    vim.notify("Installing JDTLS language server...", vim.log.levels.INFO)
+    installer.install('jdtls')
+
+    local timer = vim.loop.new_timer()
+    timer:start(2000, 750, function()
+        if servers.is_server_installed('jdtls') then
+            timer:close()
+            installer.info_window.close()
+            vim.defer_fn(function()
+                M.jdtls_setup(M.config)
+                vim.notify("JDTLS language server installed, completion should work now", vim.log.levels.INFO)
+            end, 100)
+        end
+    end)
+end
 
 local function resolve_jdtls()
-    local jdtls_path = servers.get_server_install_path('jdtls')
-    return {
-        jar = vim.fn.expand(jdtls_path .. '/plugins/org.eclipse.equinox.launcher_*.jar'),
-        config = vim.fn.expand(jdtls_path .. '/config_linux'),
-    }
+    local ok, servers = pcall(require, "nvim-lsp-installer.servers")
+    assert(ok, 'nvim-lsp-installer is not installed')
+
+    if servers.is_server_installed('jdtls') then
+        local jdtls_path = servers.get_server_install_path('jdtls')
+        return {
+            jar = vim.fn.expand(jdtls_path .. '/plugins/org.eclipse.equinox.launcher_*.jar'),
+            config = vim.fn.expand(jdtls_path .. '/config_linux'),
+        }
+    else
+        vim.loop.new_timer():start(1000, 0, vim.schedule_wrap(function()
+            download_jdtls()
+        end))
+        return false
+    end
 end
 
 local function resolve_path()
-    local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
     local jdtls_path = resolve_jdtls()
+    if not jdtls_path then
+        return false
+    end
     return {
         workspace_dir = '/tmp/workspace-root/' .. project_name,
         jdtls = jdtls_path,
@@ -20,12 +51,14 @@ local function resolve_path()
     }
 end
 
-function M.jdtls_setup(config)
-    local paths = resolve_path()
+local function lspconfig_setup(paths)
+    if not paths then
+        return
+    end
     require('lspconfig').jdtls.setup{
-        on_attach = config.on_attach,
+        on_attach = M.config.on_attach,
         cmd = {
-            config.java_exec,
+            M.config.java_exec,
             '-Declipse.application=org.eclipse.jdt.ls.core.id1',
             '-Dosgi.bundles.defaultStartLevel=4',
             '-Declipse.product=org.eclipse.jdt.ls.core.product',
@@ -49,6 +82,11 @@ function M.jdtls_setup(config)
             }
         },
     }
+end
+
+function M.jdtls_setup(config)
+    M.config = config
+    lspconfig_setup(resolve_path())
 end
 
 return M
